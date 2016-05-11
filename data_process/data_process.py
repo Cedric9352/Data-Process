79371b3b7756955ae5d7e5680d3693b188ad81d8
Cedric Cheng <nuclearbuff@hotmail.com>
2016-05-12 00:01:15 +0800

    source code V1.0

--- data_process/data_process.py
+++ data_process/data_process.py
@@ -7,10 +7,11 @@ list_dir = os.listdir(current_path)
 dir_status = False
 
 for data_file in list_dir:
-    if not data_file.endswith(".py"):
-        f_read = open(os.path.join(current_path, data_file))
-        f_list = f_read.readlines()
-        f_read.close()
+    if data_file.endswith(".py"):
+        continue
+    f_read = open(os.path.join(current_path, data_file))
+    f_list = f_read.readlines()
+    f_read.close()
 
     wave_lines = f_list[2:6]
     dir_lines = f_list[10:18]
@@ -32,6 +33,8 @@ for data_file in list_dir:
         data_dir[dir_dataList[0][0]] = float(dir_dataList[0][1])
         data_dir[dir_dataList[1][0]] = float(dir_dataList[1][1])
 
+    data_dir = sorted(data_dir.iteritems(), key=lambda x: float(x[0]), reverse=False)
+
     if not dir_status:
         parent_path = os.path.dirname(current_path)
         os.chdir(parent_path)
@@ -43,18 +46,18 @@ for data_file in list_dir:
 
         os.chdir(os.path.join(parent_path, 'MatlabData'))
         dir_status = True
+
     new_path = os.getcwd()
 
-    for new_file in list_dir:
-        new_file = new_file.split(r".")[0]+".txt"
-        f_write = open(os.path.join(new_path, new_file), 'w')
+    data_file = data_file.split(r".")[0]+".txt"
+    f_write = open(os.path.join(new_path, data_file), 'w')
 
-        keys_wave = data_wave.keys()
-        keys_dir = data_dir.keys()
+    for eachKey in data_wave:
+        f_write.write("%s\t%f\n" % (eachKey, data_wave[eachKey]))
 
-        for i in range(8):
-            for eachList in keys_wave, keys_dir:
-                f_write.write("%s\t%d\t\t\t\t" % (eachKey, data_wave[eachKey]))
-                f_write.write("%d\t%d\n" % (eachKey, data_dir[eachKey]))
+    f_write.write("-"*20+"\n")
 
-        f_write.close()
+    for eachTuple in data_dir:
+        f_write.write("%f\t%f\n" % (float(eachTuple[0]), eachTuple[1]))
+
+    f_write.close()
